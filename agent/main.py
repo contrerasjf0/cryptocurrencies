b8d89169ca41c9d53502d7be9da1d6920400395f
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-02-27 06:16:40 -0600

    The methods to process the data returned by the API was implement an also the data storage methods was implemented

--- agent/main.py
+++ agent/main.py
@@ -18,3 +18,61 @@ def get_cryptocurrencies_from_api():
         return result
 
     raise Exception('API Error')
+
+def get_hash(value):
+    from hashlib import sha512
+    return sha512(value.encode('utf-8')).hexdigest()
+
+def first_element(elements):
+    return elements [0]
+
+def get_ticker_hash(ticker_data):
+    from collections import OrdereDict
+    ticker_data = OrdereDict(
+        sorted(
+            ticker_data.items(),
+            key = first_element
+
+            )
+        )
+
+    ticker_value = ''
+
+    for _, value in ticker_data.items():
+        ticker_value += str(value)
+
+    return get_hash(ticker_value)
+    
+def check_if_exists(db_connection, ticker_data):
+    ticker_hash = get_ticker_hash(ticker_data)
+    
+    if db_connection.tickers.find_one({'ticker_hash': ticker_hash}):
+        return True
+    return False
+
+# Save ticket
+def save_ticker(db_connection, ticker_data = None):
+    if not ticker_data:
+        return False
+
+    if check_if_exists(db_connection, ticker_data):
+        return False
+
+    # Convert to integers
+    ticker_hash = get_ticker_hash(ticker_data)
+    ticker_data['ticker_hash'] = ticker_hash
+    ticker_data['rank'] = int(ticker_data['rank'])
+    ticker_data['last_updated'] = int(ticker_data['last_updated'])
+
+    # Insert a document into the MongoDB collection
+    db_connection.tickers.insert_one(ticker_data)
+    return True
+
+if __name__ == '__main__':
+    connection = get_db_connection('mongodb://localhost:27017/')
+    tickers = get_cryptocurrencies_from_api()
+    
+    for ticker in tickers:
+        save_ticker(connection, ticker)
+    
+    print('Tickers almacenados')
